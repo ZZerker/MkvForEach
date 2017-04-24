2ff4339d1da425a70261ed1480c176b45b3d89c1
Steffen Wolke <s.wolke@dascus.de>
2017-04-24 11:47:09 +0200

    conv multiple files

--- MkvForEachNetCore/Program.cs
+++ MkvForEachNetCore/Program.cs
@@ -8,7 +8,7 @@ namespace MkvForEachNetCore
     internal class Program
     {
         //cmd = r"C:/Users/steff/Downloads/mkvtoolnix-64bit-10.0.0\mkvmerge.exe --ui-language de --output 'D:\VID_20170403_085827.mkv' --language 0:und --compression 0:zlib --language 1:und 'D:\Temp\TestVideos\VID_20170403_085827.mp4' --track-order 0:0,0:1"
-        public static readonly string[] wantedFileEndings = new string[]{"avi","mp4"};
+        public static readonly string[] wantedFileEndings = new string[]{".avi",".mp4"};
 
 
         private static void Main(string[] args)
@@ -29,11 +29,14 @@ namespace MkvForEachNetCore
             }
 
             var convertableFiles = GetConvertableFiles(dir);
+            foreach(var convertableFile in convertableFiles)
+            {
+                var s = @"C:/Users/steff/Downloads/mkvtoolnix-64bit-10.0.0\mkvmerge.exe --ui-language de --output '"+convertableFile.TrimEnd(Path.GetExtension(convertableFile).ToCharArray())+".mkv' --language 0:und --compression 0:zlib --language 1:und '"+convertableFile+"' --track-order 0:0,0:1";
+                var strCmdText = $@"/C {s}";
+                Process.Start("Powershell.exe", strCmdText);
+            }
 
-
-            var s =@"C:/Users/steff/Downloads/mkvtoolnix-64bit-10.0.0\mkvmerge.exe --ui-language de --output 'D:\VID_20170403_085827.mkv' --language 0:und --compression 0:zlib --language 1:und 'D:\Temp\TestVideos\1.mp4' --track-order 0:0,0:1";
-            var strCmdText = $@"/C {s}";
-            Process.Start("Powershell.exe", strCmdText);
+           
 
             
         }
